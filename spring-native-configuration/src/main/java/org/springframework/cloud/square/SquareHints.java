f39039dcc52b513ace243e8a5f722f236df30a49
Josh Long <josh@joshlong.com>
2021-07-05 21:50:16 -0700

    up

--- spring-native-configuration/src/main/java/org/springframework/cloud/square/SquareHints.java
+++ spring-native-configuration/src/main/java/org/springframework/cloud/square/SquareHints.java
@@ -1,16 +1,6 @@
 package org.springframework.cloud.square;
 
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClientConfiguration;
-import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
-import org.springframework.cloud.square.retrofit.DefaultRetrofitClientConfiguration;
-import org.springframework.cloud.square.retrofit.RetrofitClientFactoryBean;
-import org.springframework.cloud.square.retrofit.core.AbstractRetrofitClientFactoryBean;
-import org.springframework.cloud.square.retrofit.core.RetrofitClientSpecification;
-import org.springframework.nativex.hint.AccessBits;
-import org.springframework.nativex.hint.NativeHint;
-import org.springframework.nativex.hint.TypeHint;
 import org.springframework.nativex.type.NativeConfiguration;
-import retrofit2.Retrofit;
 
 /**
 	* Provides hints for commonly required dependencies when using Spring Cloud Square Retrofit.
